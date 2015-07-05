a99fe41b992513d865eac5929612b3e799139b98
Ken Rinfeld B. Garcia <kenrinfeld.cybertechcebu@gmail.com>
2015-07-05 16:01:18 -0700

    Thumbnail Categories

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150703232537) do
+ActiveRecord::Schema.define(version: 20150705201715) do
 
   create_table "admins", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -46,6 +46,9 @@ ActiveRecord::Schema.define(version: 20150703232537) do
     t.integer  "price"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "category_id"
   end
+
+  add_index "products", ["category_id"], name: "index_products_on_category_id", using: :btree
 
 end
